7064115f035ef00bfd4eec83820b2ea5afae86df
Mario Morales <marioirving.morales@gmail.com>
2017-02-08 18:17:43 -0600

    'Practicas3A'

--- practicas/P03/Exercise3.java
+++ practicas/P03/Exercise3.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Exercise3 {
 
   public static void main(String[] args) {
@@ -13,28 +14,28 @@ public class Exercise3 {
         System.out.println("El numero " + numero + " le corresponde el mes de enero");
         else if (numero==2)
           System.out.println("El numero " + numero + " le corresponde el mes de febrero");
-            else if (numero==3)
-                System.out.println("El numero " + numero + " le corresponde el mes de marzo");
-                    else if (numero==4)
-                      System.out.println("El numero " + numero + " le corresponde el mes de abril");
-                        else if (numero==5)
-                          System.out.println("El numero " + numero + " le corresponde el mes de mayo");
-                            else if (numero==6)
-                              System.out.println("El numero " + numero + " le corresponde el mes de junio");
-                                else if (numero==7)
-                                  System.out.println("El numero " + numero + " le corresponde el mes de julio");
-                                    else if (numero==8)
-                                      System.out.println("El numero " + numero + " le corresponde el mes de agosto");
-                                        else if (numero==9)
-                                          System.out.println("El numero " + numero + " le corresponde el mes de septiembre");
-                                            else if (numero==10)
-                                              System.out.println("El numero " + numero + " le corresponde el mes de octubre");
-                                                else if (numero==11)
-                                                  System.out.println("El numero " + numero + " le corresponde el mes de noviembre");
-                                                    else if (numero==12)
-                                                      System.out.println("El numero " + numero + " le corresponde el mes de diciembre");
-                                                        else
-                                                          System.out.println("Lo siento, no es un numero valido");
+        else if (numero==3)
+          System.out.println("El numero " + numero + " le corresponde el mes de marzo");
+        else if (numero==4)
+          System.out.println("El numero " + numero + " le corresponde el mes de abril");
+        else if (numero==5)
+          System.out.println("El numero " + numero + " le corresponde el mes de mayo");
+        else if (numero==6)
+          System.out.println("El numero " + numero + " le corresponde el mes de junio");
+        else if (numero==7)
+          System.out.println("El numero " + numero + " le corresponde el mes de julio");
+        else if (numero==8)
+          System.out.println("El numero " + numero + " le corresponde el mes de agosto");
+        else if (numero==9)
+          System.out.println("El numero " + numero + " le corresponde el mes de septiembre");
+        else if (numero==10)
+          System.out.println("El numero " + numero + " le corresponde el mes de octubre");
+        else if (numero==11)
+          System.out.println("El numero " + numero + " le corresponde el mes de noviembre");
+        else if (numero==12)
+          System.out.println("El numero " + numero + " le corresponde el mes de diciembre");
+        else
+          System.out.println("Lo siento, no es un numero valido");
   }
 
 }
---
+++ practicas/P04A/Exercise1.java
@@ -0,0 +1,15 @@
+public Exercise1 {
+
+  public static boolean incorrectPasscodeAttempts(int passcode, int attempts){
+
+    int  contador, i;
+
+    ArrayList<Integer> intentos= new ArrayList<Integer>();
+    intentos.add(attempts);
+
+      
+
+  }
+
+
+}
