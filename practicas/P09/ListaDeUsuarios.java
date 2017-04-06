637916694ac7b243dd8d0040027e26b3676b8144
Mario Morales <marioirving.morales@gmail.com>
2017-04-06 10:27:31 -0500

    Add files via upload

---
+++ practicas/P09/ListaDeUsuarios.java
@@ -0,0 +1,31 @@
+public class ListaDeUsuarios {
+
+	private String usuario;
+	private String contrasena;
+
+	public ListaDeUsuarios (){
+
+	}
+
+	public ListaDeUsuarios (String usuario_param, String contrasena_param){
+		this.usuario=usuario_param;
+		this.contrasena=contrasena_param;
+	}
+
+	public void setUsuario(String usuario_param){
+		this.usuario=usuario_param;
+	}
+
+	public String getUsuario(){
+		return this.usuario;
+	}
+	
+	public void setContrasena(String contrasena_param){
+		this.contrasena=contrasena_param;
+	}
+
+	public String getContrasena(){
+		return this.contrasena;
+	}
+
+}
